442c20759e5dd09326bfc72131ce29b0c9d80d36
tonyveetu <uteev@bk.ru>
2018-09-07 08:20:29 +0300

    add self

--- src/main/java/uteevbkru/struct/Artist.java
+++ src/main/java/uteevbkru/struct/Artist.java
@@ -1,12 +1,18 @@
 package uteevbkru.struct;
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
+import java.awt.geom.AffineTransform;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Stream;
 
 /**
  *  Артист может быть один, а может быть несколько!
  */
 public class Artist {
+    private Artist self;
     private String name;
     private int age;
     private List<Artist> artists;
@@ -14,7 +20,8 @@ public class Artist {
     public Artist(String name, int age) {
         this.name = name;
         this.age = age;
-        artists = new ArrayList<>();
+        self = this;
+        System.out.println(self.getName());
     }
 
     public Artist(List<Artist> artists) {
@@ -30,6 +37,12 @@ public class Artist {
     }
 
     public List<Artist> getMembers() {
-        return artists;
+        if (artists == null) {
+            return artists;
+        } else {
+            artists = new ArrayList<>();
+            artists.add(self);
+            return artists;
+        }
     }
 }
