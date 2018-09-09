9d1cbf12f97146a73d334048b3da12733b805485
tonyveetu <uteev@bk.ru>
2018-09-09 21:57:08 +0300

    ent

---
+++ src/main/java/uteevbkru/chapters/Chapter3.java
@@ -0,0 +1,28 @@
+package uteevbkru.chapters;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Chapter3 {
+
+
+    public static int AddUp(Stream<Integer> numbers) {
+        return numbers.reduce(0, (acc, x) -> acc + x);
+    }
+
+    public static void main(String[] args) {
+        /** 1.a check function AddUp */
+        List<Integer> ints = Stream.of(1,2,3,4,5)
+                .collect(toList());
+        int sym = AddUp(ints.stream());
+        System.out.println(sym);
+
+        /** 1.b In class Album */
+
+        /** 1.c In class Album */
+
+
+    }
+}
